dbbd7891b25a7a1b0bc96fde0e939d8d94429d9f
lena <lena.savin22@gmail.om>
2024-02-07 01:21:58 +0200

    chart improvements

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-export file=$1
-python src/main.py -f files --filename "${file}" -d true
+python src/main.py -f files --filename "${FILE_ID}" -d true
 
-if [[ -f "${file}_output.csv" ]]; then
-    oci os object put --bucket-name Keystone-Quest-Outcome --file "${file}_output.csv"
-    oci os object put --bucket-name Keystone-Quest-Outcome --file "${file}_summery.csv"
+tail -n 2 proccess.log | grep -oP 'Total runtime: \K\d+\.\d+' > ${FILE_ID}_runtime.txt
+if [[ -f "${FILE_PATH}/${FILE_ID}_output.csv" ]]; then
+    oci os object put --bucket-name Keystone-Quest-Outcome --file "${FILE_PATH}/${FILE_ID}_output.csv"
+    oci os object put --bucket-name Keystone-Quest-Outcome --file "${FILE_PATH}/${FILE_ID}_summary.csv"
+    oci os object put --bucket-name Keystone-Quest-Outcome --file  ${FILE_ID}_runtime.txt
+else
+    echo "nothing to push"
 fi
